eb72342304b2ae542b91f0aadece68166ed56be8
Gabbehellgren <oscar.hellgren2007@gmail.com>
2025-11-20 08:15:26 +0100

    index finished (;

--- script/plock.js
+++ script/plock.js
@@ -1,3 +1,18 @@
+const loadingtime = 1000
+
+const loader = document.getElementsByClassName("loader")
+setTimeout(() => {
+  for (const el of document.getElementsByClassName("loader")) {
+    el.style.animation = "fade 0.5s linear"
+    el.style.opacity = "0"
+
+    setTimeout(() => {
+        el.style.display = "none"
+    }, 500)
+  }
+}, loadingtime);
+
+
 const activeLink = document.querySelector('nav .active');
 const otherLinks = document.querySelectorAll('nav a:not(.active)');
 
